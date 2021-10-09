f0106e551cb7f87009719a384ec23dbee04715b1
KarthikManoj <manojdeen12@gmail.com>
2021-10-09 12:54:23 +0530

    added visual test

---
+++ cypress/integration/todomvc-visual.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+import * as todoPage from '../page-objects/todo-page'
+
+describe('visual-validation', () => {
+    before(() => todoPage.navigate())
+
+    beforeEach(() => cy.eyesOpen({
+        appName: 'TODO MVC', 
+        batchName: 'MY TODO MVC',
+        browser: [
+            {name: 'chrome', width: 1024, height:768},
+            {name: 'chrome', width: 800, height:600},
+            {name: 'firefox', width: 1024, height:768},
+            {deviceName: 'IphoneX'},
+        ]
+    }))   
+    afterEach(() => cy.eyesClose())
+
+    it( 'should look good', () => {
+        cy.eyesCheckWindow('empty todo list')
+
+        todoPage.addTodo('Clean room')
+        todoPage.addTodo('Learn JavaScript')
+
+        cy.eyesCheckWindow('two todos')
+        
+
+        todoPage.toggleTodo(0)
+        cy.eyesCheckWindow('mark as completed')
+    } )
+
+})
